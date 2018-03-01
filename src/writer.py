ea4ee8845ee64f94f63c64ef09f0d356a6dbcc62
NikiKopp <nkopp@student.tugraz.at>
2018-03-01 19:46:39 +0100

    update writer

--- src/writer.py
+++ src/writer.py
@@ -1,10 +1,16 @@
 import numpy as np
 
 class Writer:
-    def __init__(self, object, file_name):
+    def __init__(self, vehicle_list, file_name):
         file = open(file_name, mode='w')
         text = ""
 
+        for vehicle in range(len(vehicle_list)):
+            text += str(len(vehicle))
+            for ride in range(len(vehicle)):
+                text += ' ' + str(ride)
+            if vehicle != len(vehicle_list) - 1:
+                text += '\n'
 
         file.write(text)
         file.close()

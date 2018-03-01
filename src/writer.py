8c4a8330b8d392ea180760529b6ffffccd8ca20a
NikiKopp <nkopp@student.tugraz.at>
2018-03-01 20:10:22 +0100

    FIXED EVERYTHING

--- src/writer.py
+++ src/writer.py
@@ -5,9 +5,9 @@ class Writer:
         file = open(file_name, mode='w')
         text = ""
 
-        for vehicle in range(len(vehicle_list)):
+        for vehicle in vehicle_list:
             text += str(len(vehicle))
-            for ride in range(len(vehicle)):
+            for ride in vehicle:
                 text += ' ' + str(ride)
             if vehicle != len(vehicle_list) - 1:
                 text += '\n'
